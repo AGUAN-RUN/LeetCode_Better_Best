ae00a9e9bd8e14145277504160fbbe18eda3a756
sin <45915290+javaguan@users.noreply.github.com>
2020-03-16 21:19:12 +0800

    2020-03-16  5

---
+++ src/chapter1/section1/SearchInRotatedSortedArrayII2.java
@@ -0,0 +1,43 @@
+package chapter1.section1;
+
+
+class SearchInRotatedSortedArrayII2 {
+    //对于第一次提交的优化，优化对于重复元素不多的元素进行二分处理
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return false;
+
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[start] == nums[mid]) {
+                start++;
+                continue;
+            }
+
+            if (nums[start] < nums[mid]) {
+
+                if (nums[mid] > target && nums[start] <= target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+
+                if (nums[mid] < target && nums[end] >= target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+
+                }
+            }
+        }
+
+        return false;
+
+    }
+}
