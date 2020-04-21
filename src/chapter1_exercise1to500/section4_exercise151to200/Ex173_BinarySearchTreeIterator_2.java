2940e436b93eb8b44b2eb4e811cd279254a442a0
sin <45915290+javaguan@users.noreply.github.com>
2020-04-21 23:25:15 +0800

    2020-04-21 4

--- src/chapter1_exercise1to500/section4_exercise151to200/Ex173_BinarySearchTreeIterator_2.java
+++ src/chapter1_exercise1to500/section4_exercise151to200/Ex173_BinarySearchTreeIterator_2.java
@@ -2,6 +2,7 @@ package chapter1_exercise1to500.section4_exercise151to200;
 
 import commons.TreeNode;
 
+import java.util.ArrayDeque;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -19,31 +20,40 @@ public class Ex173_BinarySearchTreeIterator_2 {
     /*ArrayDeque是Deque接口的一个实现，使用了可变数组，所以没有容量上的限制。
     同时，ArrayDeque是线程不安全的，在没有外部同步的情况下，不能再多线程环境下使用。
     ArrayDeque是Deque的实现类，可以作为栈来使用，效率高于Stack；*/
-
-
-    private LinkedList<Integer> roots;
-    private Iterator<Integer> iterator;
-
+    private ArrayDeque<TreeNode>roots;
     public Ex173_BinarySearchTreeIterator_2(TreeNode root) {
-        roots = new LinkedList<>();
+       roots=new ArrayDeque<>();
+       while(root!=null){
+           //roots.push(root);
+           roots.addLast(root);
+           root=root.left;
+       }
 
-        iterator = roots.listIterator();
     }
 
     /**
      * @return the next smallest number
      */
     public int next() {
-        return iterator.next();
+        TreeNode root=roots.pollLast();
+        int value=root.val;
+        if(root.right!=null){
+            root=root.right;
+            while(root!=null){
+                roots.addLast(root);
+                root=root.left;
+            }
+        }
+        return value;
     }
 
     /**
      * @return whether we have a next smallest number
      */
     public boolean hasNext() {
-        return iterator.hasNext();
+        return !roots.isEmpty();
     }
-    //通过查找树的中序遍历保存元素到链表中
+
 
 
 }
