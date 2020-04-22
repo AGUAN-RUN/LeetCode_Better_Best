13f5928164f021366ba3c07bc95570edb3c59fed
AGUAN-RUN <2656141004@qq.com>
2020-04-22 11:58:59 +0800

    2020-04-22 3

--- src/SQL_EXERCISE/Ex175_CombineTwoTables.sql
+++ src/SQL_EXERCISE/Ex175_CombineTwoTables.sql
@@ -25,4 +25,7 @@ AddressId is the primary key column for this table.
 Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
 
 FirstName, LastName, City, State*/
+/*MYSQL*/
+SELECT PSN.FIRSTNAME,PSN.LASTNAME,ADR.CITY,ADR.STATE FROM PERSON PSN LEFT JOIN ADDRESS ADR ON PSN.PERSONID=ADR.PERSONID;
+/*ORACLE   oarcle执行一个测试用例正确  全部7个测试用例超时？？*/
 SELECT PSN.FIRSTNAME,PSN.LASTNAME,ADR.CITY,ADR.STATE FROM PERSON PSN LEFT JOIN ADDRESS ADR ON PSN.PERSONID=ADR.PERSONID;
