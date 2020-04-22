126e77669dc1c46aba28b91939e14580442d197e
AGUAN-RUN <2656141004@qq.com>
2020-04-22 15:14:48 +0800

    2020-04-22 6

--- src/chapter1_exercise1to500/section4_exercise151to200/Ex179_LargestNumber.java
+++ src/chapter1_exercise1to500/section4_exercise151to200/Ex179_LargestNumber.java
@@ -4,6 +4,8 @@ Given a list of non negative integers, arrange them such that they form the larg
 
 Note: The result may be very large, so you need to return a string instead of an integer.
 */
+
+//常用的开发工具快捷键平时还是要多用用，用习惯了提高效率真的不只是一点点
 public class Ex179_LargestNumber {
     //设计一个针对该问题特性的数组排序方法，完成数组排序后，直接遍历拼接到StringBuilder中，最后输出结果String
     public String largestNumber(int[] nums) {
