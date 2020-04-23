96b6c49268556e69a1741d3cfc1442ed2884c740
AGUAN-RUN <2656141004@qq.com>
2020-04-23 12:28:21 +0800

    2020-04-23 4

---
+++ src/SQL_EXERCISE/Ex182_DuplicateEmails.sql
@@ -0,0 +1,21 @@
+/*SQL架构
+Write a SQL query to find all duplicate emails in a table named Person.
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+For example, your query should return the following for the above table:
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+*/
+# Write your MySQL query statement below
+select x.email from (select email,count(email) num from person group by email)x where x.num>1;
+
+#oracle same as mysql
---
+++ src/SQL_EXERCISE/Ex184_DepartmentHighestSalary.sql
@@ -0,0 +1,53 @@
+/*The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+
++----+-------+--------+--------------+
+| Id | Name  | Salary | DepartmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 70000  | 1            |
+| 2  | Jim   | 90000  | 1            |
+| 3  | Henry | 80000  | 2            |
+| 4  | Sam   | 60000  | 2            |
+| 5  | Max   | 90000  | 1            |
++----+-------+--------+--------------+
+The Department table holds all departments of the company.
+
++----+----------+
+| Id | Name     |
++----+----------+
+| 1  | IT       |
+| 2  | Sales    |
++----+----------+
+Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, your SQL query should return the following rows (order of rows does not matter).
+
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Jim      | 90000  |
+| Sales      | Henry    | 80000  |
++------------+----------+--------+*/
+# Write your MySQL query statement below
+# 以下写法正确，但是在leetcode提交返回超时
+select  dept.name Department,emp.name Employee, emp.salary  Salary
+from employee emp
+left join department dept on dept.id = emp.departmentid
+where emp.salary = (select max(emp2.salary) from employee emp2 where emp2.departmentid=dept.id) order by emp.id;
+
+# 以下使用 in 的写法  效率和以上写法相比要高一些，提交通过了  击败  80%？？？
+SELECT
+    Department.name AS 'Department',
+    Employee.name AS 'Employee',
+    Salary
+FROM
+    Employee
+        JOIN
+    Department ON Employee.DepartmentId = Department.Id
+WHERE
+    (Employee.DepartmentId , Salary) IN
+    (   SELECT
+            DepartmentId, MAX(Salary)
+        FROM
+            Employee
+        GROUP BY DepartmentId
+	)
+
