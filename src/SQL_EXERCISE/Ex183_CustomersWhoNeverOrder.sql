5f78898146fadd0626d6f365c64a2a1c338e58dd
AGUAN-RUN <2656141004@qq.com>
2020-04-23 11:25:46 +0800

    2020-04-23 3

---
+++ src/SQL_EXERCISE/Ex183_CustomersWhoNeverOrder.sql
@@ -0,0 +1,32 @@
+/*SQL架构
+Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+Table: Customers.
+
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Table: Orders.
+
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Using the above tables as example, return the following:
+
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+*/
+
+# Write your MySQL query statement below   ORACLE is same as this
+select t.name Customers from customers t where t.id not in (select customerId from orders where customerId is not null);
