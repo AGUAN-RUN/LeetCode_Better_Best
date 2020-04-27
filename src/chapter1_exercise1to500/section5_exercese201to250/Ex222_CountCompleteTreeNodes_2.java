821c055a868ab906f46715e9efba9e0b6fa10c64
AGUAN-RUN <2656141004@qq.com>
2020-04-27 17:56:33 +0800

    2020-04-27 7

---
+++ src/chapter1_exercise1to500/section5_exercese201to250/Ex222_CountCompleteTreeNodes_2.java
@@ -0,0 +1,34 @@
+package chapter1_exercise1to500.section5_exercese201to250;
+
+import commons.TreeNode;
+import sun.reflect.generics.tree.Tree;
+
+/*
+Given a complete binary tree, count the number of nodes.
+
+Note:
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Example:
+
+Input:
+    1
+   / \
+  2   3
+ / \  /
+4  5 6
+
+Output: 6
+*/
+public class Ex222_CountCompleteTreeNodes_2 {
+
+    //性质1：获得二叉树的高度h以及缺失的叶子节点m   2^h-1-m  为该满二叉树的节点数量
+    //性质2：左子树叶子节点不满，右子树叶子节点坑定也不满，左子树叶子节点满，右子树叶子节点不一定满
+    public int countNodes(TreeNode root) {
+        //最左边的节点高度树的高度
+        int hight=0;
+        TreeNode =
+
+    }
+}
