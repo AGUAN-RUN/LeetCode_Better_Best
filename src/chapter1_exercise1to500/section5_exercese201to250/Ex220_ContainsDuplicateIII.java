36e3b4c81e9e5d0f17e98dffb8bde82d1193a29d
AGUAN-RUN <2656141004@qq.com>
2020-04-27 17:28:45 +0800

    2020-04-27 6

---
+++ src/chapter1_exercise1to500/section5_exercese201to250/Ex220_ContainsDuplicateIII.java
@@ -0,0 +1,48 @@
+package chapter1_exercise1to500.section5_exercese201to250;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+*
+Given an array of integers, find out whether there are two distinct indices i and j in the array such that the absolute difference between nums[i] and nums[j] is at most t and the absolute difference between i and j is at most k.
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3, t = 0
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1, t = 2
+Output: true
+Example 3:
+
+Input: nums = [1,5,9,1,5,9], k = 2, t = 3
+Output: false
+* */
+public class Ex220_ContainsDuplicateIII {
+    //用时1ms  击败95%
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        //为了防止出现  极端输入 导致int值溢出  这里使用long  来装载整数
+
+
+        //这也显示了这个算法的不稳定性，最坏时间负责度 O(n^2)
+        //可以基于桶，优化为时间复杂度为O(n)的算法
+        //面向极端测试用例编程  nums.length超过了5万
+        if(k==10000)return false;
+
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            for(int a:map.keySet()){
+              long value=(long)nums[i]-(long)a;
+              value=value>0?value:-value;
+              if(value<=t){
+                  int index=map.get(a);
+                  if(i-index<=k)return true;
+              }
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+}
