e4515cfa86dcffbada2bd93e0864dc81ecf5872a
AGUAN-RUN <2656141004@qq.com>
2020-04-29 14:34:29 +0800

    2020-04-29 5

---
+++ src/chapter1_exercise1to500/section5_exercese201to250/Ex240_SearchA2DMatrixII.java
@@ -0,0 +1,42 @@
+package chapter1_exercise1to500.section5_exercese201to250;
+/*
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+
+Given target = 20, return false.
+* */
+public class Ex240_SearchA2DMatrixII {
+
+    //执行用时6ms  击败99.83%
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix==null||matrix.length==0||matrix[0].length==0||target<matrix[0][0]||target>matrix[matrix.length][matrix[0].length])return false;
+        int m=matrix.length-1;
+        int n=0;
+        //由于左下角的元素对于一列是最大，对应一行是最小，可以根据目标值和这个元素的大小来对比
+        //如果相等则返回结果，如果小于 列号减去1  如果大于行号加一
+        while(m>=0&&n<matrix[0].length){
+            if(target==matrix[m][n]){
+                return true;
+            }else if(target<matrix[m][n]){
+                m--;
+            }else {
+                n++;
+            }
+        }
+        return false;
+    }
+}
