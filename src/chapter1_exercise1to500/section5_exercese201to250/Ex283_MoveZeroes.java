34eee645a652143a56d58680e0158d09b7a703ba
AGUAN-RUN <2656141004@qq.com>
2020-04-30 14:02:43 +0800

    2020-04-30 5

---
+++ src/chapter1_exercise1to500/section5_exercese201to250/Ex283_MoveZeroes.java
@@ -0,0 +1,33 @@
+package chapter1_exercise1to500.section5_exercese201to250;
+/*
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+* */
+public class Ex283_MoveZeroes {
+    //用时0ms  击败100%  时间复杂度 O（n） 空间复杂度 O（1）
+    public void moveZeroes(int[] nums) {
+        if(nums==null||nums.length==0)return;
+        //从左往右遍历数组  遇到不是0的放 num[left]  left指向数组下一个元素
+        int left=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                if(left!=i){
+                    nums[left]=nums[i];
+                }
+                left++;
+            }
+        }
+        //left 之后的全部
+        for(int i=left;i<nums.length;i++){
+            nums[i]=0;
+        }
+    }
+}
---
+++ src/chapter1_exercise1to500/section5_exercese201to250/Ex290_WordPattern.java
@@ -0,0 +1,52 @@
+package chapter1_exercise1to500.section5_exercese201to250;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+
+Example 1:
+
+Input: pattern = "abba", str = "dog cat cat dog"
+Output: true
+Example 2:
+
+Input:pattern = "abba", str = "dog cat cat fish"
+Output: false
+Example 3:
+
+Input: pattern = "aaaa", str = "dog cat cat dog"
+Output: false
+Example 4:
+
+Input: pattern = "abba", str = "dog dog dog dog"
+Output: false
+Notes:
+You may assume pattern contains only lowercase letters, and str contains lowercase letters that may be separated by a single space.
+* */
+public class Ex290_WordPattern {
+    public boolean wordPattern(String pattern, String str) {
+        if(pattern==null||pattern.length()==0||str==null||str.length()==0)return false;
+        //使用一个26长度的String数组  来存储pattern的一个字符与str的一个子串之间的映射关系
+        String[]map=new String[26];
+        Set<String> set=new HashSet<>();
+        //如果不用API的话很简单
+        char[]cp=pattern.toCharArray();
+        String[]strings=str.split(" ");
+        if(cp.length!=strings.length)return false;
+        for(int i=0;i<cp.length;i++){
+            if(map[cp[i]-'a']==null){
+                //如果这个字符串没有和其他字符匹配，添加映射 否则返回false
+                if(set.contains(strings[i]))return false;
+                set.add(strings[i]);
+                map[cp[i]-'a']=strings[i];
+            }else {
+                if(!map[cp[i]-'a'].equals(strings[i]))return false;
+            }
+        }
+        return true;
+    }
+}
