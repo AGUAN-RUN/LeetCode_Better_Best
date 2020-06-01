f0771ed34c02531a636c25c2ab6abe3186cf4d13
AGUAN-RUN <2656141004@qq.com>
2020-06-01 09:19:12 +0800

    2020-6-1 3

---
+++ src/chapter1_exercise1to500/section8_exercise351to400/Ex394_DecodeString.java
@@ -0,0 +1,27 @@
+package chapter1_exercise1to500.section8_exercise351to400;
+/*
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+Examples:
+
+s = "3[a]2[bc]", return "aaabcbc".
+s = "3[a2[c]]", return "accaccacc".
+s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+* */
+public class Ex394_DecodeString {
+    public String decodeString(String s) {
+        if(s == null || s.equals(""))return s;
+        char[] strs = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i < strs.length; i++){
+            int index = strs[i]-'0';
+
+        }
+    }
+}
