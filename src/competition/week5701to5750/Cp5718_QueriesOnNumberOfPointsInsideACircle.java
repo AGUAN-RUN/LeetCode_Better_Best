589158b13dbf22fdbaf74af0e02347fa9d5fa6a2
AGUAN-RUN <2656141004@qq.com>
2021-04-18 00:35:10 +0800

    2021-04-18 2

---
+++ src/competition/week5701to5750/Cp5718_QueriesOnNumberOfPointsInsideACircle.java
@@ -0,0 +1,81 @@
+package competition.week5701to5750;
+/*英文题述*/
+/*
+You are given an array points where points[i] = [xi, yi] is the coordinates of the ith point on a 2D plane. Multiple points can have the same coordinates.
+You are also given an array queries where queries[j] = [xj, yj, rj] describes a circle centered at (xj, yj) with a radius of rj.
+For each query queries[j], compute the number of points inside the jth circle. Points on the border of the circle are considered inside.
+Return an array answer, where answer[j] is the answer to the jth query.
+
+
+Example 1:
+Input: points = [[1,3],[3,3],[5,3],[2,2]], queries = [[2,3,1],[4,3,1],[1,1,2]]
+Output: [3,2,2]
+Explanation: The points and circles are shown above.
+queries[0] is the green circle, queries[1] is the red circle, and queries[2] is the blue circle.
+
+Example 2:
+Input: points = [[1,1],[2,2],[3,3],[4,4],[5,5]], queries = [[1,2,2],[2,2,2],[4,3,2],[4,3,3]]
+Output: [2,3,2,4]
+Explanation: The points and circles are shown above.
+queries[0] is green, queries[1] is red, queries[2] is blue, and queries[3] is purple.
+ 
+
+Constraints:
+
+1 <= points.length <= 500
+points[i].length == 2
+0 <= x​​​​​​i, y​​​​​​i <= 500
+1 <= queries.length <= 500
+queries[j].length == 3
+0 <= xj, yj <= 500
+1 <= rj <= 500
+All coordinates are integers.
+* */
+
+/*中文题述*/
+/*
+给你一个数组 points ，其中 points[i] = [xi, yi] ，表示第 i 个点在二维平面上的坐标。多个点可能会有 相同 的坐标。
+同时给你一个数组 queries ，其中 queries[j] = [xj, yj, rj] ，表示一个圆心在 (xj, yj) 且半径为 rj 的圆。
+对于每一个查询 queries[j] ，计算在第 j 个圆 内 点的数目。如果一个点在圆的 边界上 ，我们同样认为它在圆 内 。
+请你返回一个数组 answer ，其中 answer[j]是第 j 个查询的答案。
+
+ 
+示例 1：
+输入：points = [[1,3],[3,3],[5,3],[2,2]], queries = [[2,3,1],[4,3,1],[1,1,2]]
+输出：[3,2,2]
+解释：所有的点和圆如上图所示。
+queries[0] 是绿色的圆，queries[1] 是红色的圆，queries[2] 是蓝色的圆。
+
+示例 2：
+输入：points = [[1,1],[2,2],[3,3],[4,4],[5,5]], queries = [[1,2,2],[2,2,2],[4,3,2],[4,3,3]]
+输出：[2,3,2,4]
+解释：所有的点和圆如上图所示。
+queries[0] 是绿色的圆，queries[1] 是红色的圆，queries[2] 是蓝色的圆，queries[3] 是紫色的圆。
+ 
+提示：
+
+1 <= points.length <= 500
+points[i].length == 2
+0 <= x​​​​​​i, y​​​​​​i <= 500
+1 <= queries.length <= 500
+queries[j].length == 3
+0 <= xj, yj <= 500
+1 <= rj <= 500
+所有的坐标都是整数。
+* */
+
+public class Cp5718_QueriesOnNumberOfPointsInsideACircle {
+
+    public int[] countPoints(int[][] points, int[][] queries) {
+        int[]res=new int[queries.length];
+        for(int i=0;i<points.length;i++){
+            for(int j=0;j<queries.length;j++){
+                if((Math.sqrt(Math.pow((points[i][0]-queries[j][0]),2)+ Math.pow((points[i][1]-queries[j][1]),2)))
+                        <= (double)(queries[j][2])){
+                    res[j]++;
+                }
+            }
+        }
+        return res;
+    }
+}
